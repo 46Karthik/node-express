a70b1eff693c87477f2da318f8eb68e66402a874
KARTHIK <104157027+46Karthik@users.noreply.github.com>
2023-12-31 21:38:58 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,12 @@
 
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
 
-router.post("/", async (req, res, next) => {
-    const postData = req.body;
-    const url = postData.URL;
-    const response = await axios.head(url, { maxRedirects: 5, validateStatus: null });
-    const modified_URL = response.request._redirectable._currentUrl;
-
-    res.json({ message: 'POST data received successfully', modified_URL: modified_URL });
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
 });
 
 module.exports = router;
